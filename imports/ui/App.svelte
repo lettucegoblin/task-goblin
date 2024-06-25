41650b8b32f29c26fc447e3314125667c6fab1ba
lettucegoblin <77356082+lettucegoblin@users.noreply.github.com>
2024-06-24 23:36:01 -0400

    chore: Add unique-names-generator npm dependency and generate room code on mount

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,15 +1,37 @@
 <script>
-  //https://m3.material.io/components/all-buttons
   import { onMount } from "svelte";
   import TodoList from "./TodoList.svelte";
   import AddTodo from "./AddTodo.svelte";
   import DarkMode from "./DarkMode.svelte";
+  import { uniqueNamesGenerator, Config, adjectives, colors, animals } from 'unique-names-generator';
+
+  const customConfig = {
+    dictionaries: [adjectives, colors],
+    separator: '-',
+    length: 2,
+  };
 
   let roomCode = "defaultRoom";
+  let isReady = false; // Control variable for rendering components
+
+  onMount(() => {
+    const params = new URLSearchParams(window.location.search);
+    const urlRoomCode = params.get('roomCode');
+    if (urlRoomCode) {
+      roomCode = urlRoomCode;
+      isReady = true; // Set ready to true as the room code is already set
+    } else {
+      roomCode = uniqueNamesGenerator(customConfig);
+      window.history.pushState({}, '', `?roomCode=${roomCode}`);
+      isReady = true; // Set ready to true after setting the room code
+    }
+  });
 </script>
 
 <main class="container">
   <DarkMode />
-  <AddTodo {roomCode} />
-  <TodoList {roomCode} />
+  {#if isReady}
+    <AddTodo {roomCode} />
+    <TodoList {roomCode} />
+  {/if}
 </main>
